09de937056fad280478dd1b727961775bfbdaedc
bit6tream <megapixel483@gmail.com>
2023-04-22 14:11:34 +0300

    Enforce .todo file extension

--- source/main.cxx
+++ source/main.cxx
@@ -7,13 +7,26 @@
 
 int main(int command_line_arguments_length, char *command_line_arguments[]) {
   if (command_line_arguments_length != 2) {
-    std::cerr << "Usage: todo <filename>" << std::endl;
+    std::cerr << "Usage: todo <filename.todo>" << std::endl;
     return 1;
   }
 
   const std::filesystem::path file_path(command_line_arguments[1]);
+
+  const std::filesystem::path todo_extension(".todo");
+  if (file_path.extension() != todo_extension) {
+    std::cerr << "Error: provided file is not of a .todo extension."
+              << std::endl;
+    return 1;
+  }
+
   ui::ui ui(file_path);
-  ui.main_loop();
+  try {
+    ui.main_loop();
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
 
   return 0;
 }
