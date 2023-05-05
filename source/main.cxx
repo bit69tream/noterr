6ebac2ab087c880d400fffe3d4e30ecf9289a094
bit6tream <megapixel483@gmail.com>
2023-05-05 19:39:42 +0300

    Remove command line arguments handling for now

--- source/main.cxx
+++ source/main.cxx
@@ -6,20 +6,23 @@
 #include "ui.hxx"
 
 int main(int command_line_arguments_length, char *command_line_arguments[]) {
-  if (command_line_arguments_length != 2) {
-    std::cerr << "Usage: noterr <filename.noterr>" << std::endl;
-    return 1;
-  }
+  (void) command_line_arguments;
+  (void) command_line_arguments_length;
 
-  const std::filesystem::path file_path(command_line_arguments[1]);
+  // if (command_line_arguments_length != 2) {
+  //   std::cerr << "Usage: noterr <filename.noterr>" << std::endl;
+  //   return 1;
+  // }
 
-  const std::filesystem::path noterr_extension(".noterr");
-  if (file_path.extension() != noterr_extension) {
-    std::cerr << "Error: provided file is not of a .noterr extension." << std::endl;
-    return 1;
-  }
+  // const std::filesystem::path file_path(command_line_arguments[1]);
 
-  ui::ui ui(file_path);
+  // const std::filesystem::path noterr_extension(".noterr");
+  // if (file_path.extension() != noterr_extension) {
+  //   std::cerr << "Error: provided file is not of a .noterr extension." << std::endl;
+  //   return 1;
+  // }
+
+  ui::ui ui("sample text");
   ui.main_loop();
 
   return 0;
